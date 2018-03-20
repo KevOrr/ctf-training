df60eff764fc6935d7107af3249405bfdffa72d2
Kevin Orr <kevinorr54@gmail.com>
2018-03-20 16:38:27 -0400

    Fix reading in

--- 2018-03-20/run.c
+++ 2018-03-20/run.c
@@ -13,19 +13,14 @@ int terminate_newline(char *s) {
 }
 
 int main() {
-    unsigned long long buflen = 0x10;
+    unsigned long long buflen = 0x10000;
     char *buf = malloc(buflen * sizeof(char));
 
     fputs("This is a simple hello world application.\n"
           "To save space, I'm using the same file for my RSA private key as temporary data.\n"
           "Please enter your name: ", stdout);
 
-    while (1) {
-        if (!fgets(buf, buflen, stdin) || terminate_newline(buf))
-            break;
-        buflen *= 2;
-        buf = realloc(buf, buflen * sizeof(char));
-    }
+    fgets(buf, buflen, stdin);
 
     char *cmd;
     asprintf(&cmd, "{ echo \"%s\"; cat storage; } >temp && mv temp storage && ./readflag %u", buf, strlen(buf));
