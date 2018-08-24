2c6be4f78783e757a8df835171c30e703d9d8a68
Kevin Orr <kevinorr54@gmail.com>
2018-08-24 17:40:02 -0400

    Added a benign version of the basic buffer overflow demo

--- 2018-08-24/bof_benign.c
+++ 2018-08-24/bof_benign.c
@@ -2,11 +2,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-void super_secret_shell() {
-    puts("Welcome, admin");
-    system("/bin/bash");
-}
-
 void do_hello() {
     char name[50]; // TODO support users with really long names
     fputs("Please enter your name: ", stdout);
